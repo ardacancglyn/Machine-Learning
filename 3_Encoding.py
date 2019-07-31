b6072549baafb4d5a79d9dc818cb6f2986af7260
Arda Can Çağlıyan <52882045+ardacancglyn@users.noreply.github.com>
2019-07-31 15:02:42 +0300

    Add files via upload

---
+++ 3_Encoding.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data=pd.read_csv("Social_Network_Ads.csv")
+x=data.iloc[:,2:4]
+y=data.iloc[:,4:]
+X=x.values
+Y=y.values
+
+gender=data.iloc[:,1:2].values
+
+
+from sklearn.preprocessing import LabelEncoder
+le=LabelEncoder()
+gender[:,0]=le.fit_transform(gender[:,0])
+
+from sklearn.preprocessing import OneHotEncoder
+ohe=OneHotEncoder(categorical_features="all")
+gender=ohe.fit_transform(gender).toarray()
+print(gender)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
