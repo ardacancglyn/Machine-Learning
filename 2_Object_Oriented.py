0b28c874b4299f0e912ab0d6ab7c8c827cf44f04
Arda Can Çağlıyan <52882045+ardacancglyn@users.noreply.github.com>
2019-07-31 14:55:59 +0300

    Add files via upload

---
+++ 2_Object_Oriented.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data=pd.read_csv("Social_Network_Ads.csv")
+
+male0=data.iloc[0:1,:]
+class human:
+    height=180
+    weight=70
+male0=human()
+print("15624510_Height: ")
+print(male0.height)
+print("15624510_Weight: ")
+print(male0.weight)
+
+
